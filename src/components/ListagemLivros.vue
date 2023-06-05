49f1cc25418cbe85655ffb5ef804348a80c942c0
raissalotin <rahlotin@gmail.com>
2023-06-05 11:44:28 -0300

    alterado

---
+++ src/components/ListagemLivros.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { livros } from '@/_data/livros.js'
+
+import cardlivro from './components/cardlivro.vue'
+
+</script>
+
+<template>
+    <div class="listagem-livros">
+      <cardlivro v-for="livro in livros" :key="livro.id" :livro="livro" />
+    </div>
+</template>
+
+<style scoped>
+.listagem-livros {
+  display: flex;
+  flex-wrap: wrap;
+}</style>
---
+++ src/components/carrinho.vue
@@ -0,0 +1,93 @@
+<script setup>
+import { carrinho, removerItemCarrinho, atualizaQuantidadeItem } from '@/_data/carrinho.js'
+
+function formatarPreco(preco) {
+    return 'R$' + preco.toFixed(2).replace('.', '.')
+}
+
+</script>
+<template>
+    <div class="carrinho">
+        <h2>Meu carrinho</h2>
+        <div class="wrap-carrinho">
+            <p v-if="carrinho.itens.length === 0">Seu carrinho está vazio</p>
+
+            <div v-else>
+                <div class="item-carrinho" v-for="(item, index) in carrinho.itens" :key="index">
+                    <div class="info-livro">
+                        <div class="imagem-livro">
+
+                            <img :src="item.img" class="icon-capa-livro" />
+                        </div>
+                        <div class="detalhes-livro">
+                            <div>
+                                <p>{{ item.title }}</p>
+                                <p class="info-livro-preco">{{ formatarPreco(item.price) }}/un</p>
+                            </div>
+                            <div>
+                                <p>qtde: <input type="number" v-model="item.quantidade"
+                                        @change="atualizaQuantidadeItem(item)" min="1" />
+                                </p>
+                                <button @click="removerItemCarrinho(item)">&#128465;</button>
+                                <p>total:{{ formatarPreco(item.total) }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p>Total: {{ formatarPreco(carrinho.total) }}</p>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.info-livro {
+    display: flex;
+    margin-bottom: 10px;
+}
+
+.detalhes-livro {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+}
+
+.detalhes-livro p {
+    margin: 0;
+}
+
+.detalhes-livro div {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.detalhes-livro input[type='number'] {
+    width: 50px;
+    text-align: center;
+    border: none;
+    border-bottom: 1px solid black;
+    background-color: transparent;
+    margin-left: 10px;
+}
+
+.detalhes-livro button {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    font-size: 1.5rem;
+    color: black;
+    padding: 0;
+    margin: 0
+}
+
+.info-livro-preco {
+    margin-left: auto;
+}
+
+.icon-capa-livro {
+    width: 30px;
+    margin-right: 10px;
+}
+</style>
