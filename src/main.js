e05355ea6f15a4f39d4e953b77e7b88eae12d6f0
raissalotin <rahlotin@gmail.com>
2023-07-31 11:51:29 -0300

    l

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
-import Modal from "vue-bs-modal";
+
+
 
 createApp(App).mount('#app')
